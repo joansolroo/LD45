259a7489d0905c3843b790f16cef825bc48a8097
solroo <solroo@gmail.com>
2019-10-04 18:46:21 +0200

    Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+
+    private void Start()
+    {
+        //offset = this.transform.position - target.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = target.transform.position + offset;
+    }
+}
