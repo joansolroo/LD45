658f8c9cab19452216bacc1b8afff7200e66762c
joansolroo <solroo@gmail.com>
2019-10-05 08:35:49 +0200

    Tilemap3D

---
+++ Assets/Scripts/AI/Sense.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sense : MonoBehaviour
+{
+    public LayerMask layer;
+    public Dictionary<GameObject, IPerceptible> pereceived = new Dictionary<GameObject, IPerceptible>();
+    public int perceivedCount;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            GameObject go = other.attachedRigidbody.gameObject;
+            if (layer.Contains(go.layer))
+            {
+                Debug.Log("sensed " + other.name);
+                pereceived[go] = go.GetComponent<IPerceptible>();
+                ++perceivedCount;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject go;
+        if (other.attachedRigidbody)
+        {
+            go = other.attachedRigidbody.gameObject;
+        }
+        else
+        {
+            go = other.gameObject;
+        }
+        
+        if (layer.Contains(go.layer))
+        {
+            pereceived.Remove(go);
+            --perceivedCount;
+        }
+    }
+}
---
+++ Assets/Scripts/Map/TilePrefab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.Tilemaps;
+
+
+using UnityEditor;
+public class TilePrefab : Tile
+{
+    [MenuItem("Assets/Create/TilePrefab")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<TilePrefab>();
+    }
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        bool result =  base.StartUp(position, tilemap, go);
+        if (result)
+        {
+            go.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        return result;
+    }
+}
