a54ea1250a3241711c6886b1db3d242d0f743001
solroo <solroo@gmail.com>
2019-10-05 18:10:45 +0200

    Sight based AI

---
+++ Assets/Scripts/AI/EnemyTurret.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+[DefaultExecutionOrder(-200)]
+public class EnemyTurret : MonoBehaviour, IIndividual
+{
+    [SerializeField] Controller controller;
+    [SerializeField] Sense sight;
+    public Transform target;
+    [SerializeField] float aggressivity = 0.5f;
+    [SerializeField] float aimRange = 5f;
+    [SerializeField] float shootRange = 4f;
+
+
+    float t;
+
+    [Header("debug")]
+    [SerializeField] Vector3 lastKnownPosition;
+    [SerializeField] Vector3 currentPosition;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float distance;
+
+    private void Start()
+    {
+        sight.transform.localScale = Vector3.one * aimRange;
+    }
+    private void Update()
+    {
+        Sense();
+        Think();
+        Act();
+    }
+
+    public void Sense()
+    {
+        if (target == null || !sight.IsVisible(target.gameObject))
+        {
+            if (sight.perceived.Count > 0)
+            {
+                target = sight.GetRandom().transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+
+        if (target)
+        {
+            currentPosition = target.position;
+            direction = currentPosition - this.transform.position;
+            distance = direction.magnitude;
+            if (distance < aimRange)
+            {
+                lastKnownPosition = currentPosition;
+            }
+        }
+    }
+
+    public void Think()
+    {
+
+    }
+
+    public void Act()
+    {
+
+        controller.Move(Vector3.zero);
+        bool fighting = false;
+        if (target)
+        {
+            if (distance < aimRange)
+            {
+                controller.AimAt(lastKnownPosition);
+                if (distance < shootRange)
+                {
+                    if (Random.value < aggressivity)
+                        controller.Fire1();
+                    if (Random.value < aggressivity)
+                        controller.Fire2();
+                }
+                fighting = true;
+
+            }
+
+        }
+        if (!fighting)
+        {
+            t += Time.deltaTime;
+            //controller.Move(new Vector2(Mathf.Sin(t * patrolSpeed) * patrolRange, 0));
+            float a = Time.time;
+            controller.AimAt(new Vector3(Mathf.Sin(a),0,Mathf.Cos(a)));
+        }
+    }
+
+    public void Display()
+    {
+
+    }
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    public bool Alive()
+    {
+        return controller.alive;
+    }
+}
--- Assets/Scripts/AI/Sense.cs
+++ Assets/Scripts/AI/Sense.cs
@@ -1,20 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+
 
 public class Sense : MonoBehaviour
 {
     public LayerMask layer;
-    public Dictionary<GameObject, IPerceptible> pereceived = new Dictionary<GameObject, IPerceptible>();
+    public Dictionary<GameObject, IPerceptible> perceived = new Dictionary<GameObject, IPerceptible>();
     public int perceivedCount;
+
+    public IEnumerable<TKey> RandomKeys<TKey, TValue>(IDictionary<TKey, TValue> dict)
+    {
+        List<TKey> keys = Enumerable.ToList(dict.Keys);
+        int size = dict.Count;
+        while (true)
+        {
+            yield return keys[Random.Range(0, size)];
+        }
+    }
+    public IEnumerable<TValue> RandomValues<TKey, TValue>(IDictionary<TKey, TValue> dict)
+    {
+        List<TValue> values = Enumerable.ToList(dict.Values);
+        int size = dict.Count;
+        while (true)
+        {
+            yield return values[Random.Range(0,size)];
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         GameObject go = other.attachedRigidbody ? other.attachedRigidbody.gameObject : other.gameObject;
         if (layer.Contains(go.layer))
         {
             Debug.Log("sensed " + other.name);
-            pereceived[go] = go.GetComponent<IPerceptible>();
-            perceivedCount = pereceived.Count;
+            perceived[go] = go.GetComponent<IPerceptible>();
+            perceivedCount = perceived.Count;
         }
 
     }
@@ -32,14 +54,23 @@ public class Sense : MonoBehaviour
 
         if (layer.Contains(go.layer))
         {
-            pereceived.Remove(go);
-            perceivedCount = pereceived.Count;
+            perceived.Remove(go);
+            perceivedCount = perceived.Count;
         }
+    }
+    Random rand = new Random();
+    public GameObject GetRandom()
+    {
+        return RandomKeys(perceived).First();
+    }
+    public bool IsVisible(GameObject go)
+    {
+        return perceived.ContainsKey(go);
     }
 
     private void OnDrawGizmos()
     {
-        foreach(GameObject p in pereceived.Keys)
+        foreach (GameObject p in perceived.Keys)
         {
             Gizmos.DrawLine(this.transform.position, p.transform.position);
         }
