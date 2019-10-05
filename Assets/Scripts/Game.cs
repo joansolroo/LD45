41ac11b4e3229271520ca6f147bdfdb004c8e2b9
LAINE Thibault <titwin@live.fr>
2019-10-05 18:46:28 +0200

    add game script

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject introduction;
+
+    public float gameOverDuration;
+    public GameObject respawn;
+    public Controller playerController;
+    public GameObject resetables;
+
+    private GameObject instanciatedScene;
+    private GameObject instanciatedPlayer;
+
+    public bool end;
+
+    protected virtual void Start()
+    {
+        if(introduction != null)
+            introduction.SetActive(true);
+        resetables.SetActive(false);
+        playerController.gameObject.SetActive(false);
+    }
+
+    protected virtual void LateUpdate()
+    {
+        // keep intro running if we have an intro, launch game if not or key pressed
+        if (introduction != null && introduction.activeSelf)
+        {
+            if (Input.anyKey)
+            {
+                introduction.SetActive(false);
+                Initialize();
+            }
+        }
+        else if(introduction == null)
+        {
+            Initialize();
+        }
+
+        // standard game update
+        if (introduction == null || !introduction.activeSelf)
+        {
+            if (playerController.hp <= 0 && !playerController.invincible)
+            {
+                StartCoroutine(Die());
+            }
+        }
+    }
+
+    private void Initialize()
+    {
+        instanciatedScene = Instantiate(resetables);
+        instanciatedScene.SetActive(true);
+    }
+
+    protected IEnumerator Die()
+    {
+        end = true;
+        yield return new WaitForSeconds(gameOverDuration);
+        
+        DestroyImmediate(instanciatedScene);
+        DestroyImmediate(instanciatedPlayer);
+        Initialize();
+        playerController.transform.position = respawn.transform.position;
+        playerController.Reset();
+
+        end = false;
+    }
+}
