982916af38608944a6f8fa4b768cf0b44ebef499
Unknown <titwin@live.fr>
2019-10-06 12:16:10 +0200

    minigun full support
    - and add some assets

---
+++ Assets/Scripts/Animation/MinigunAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigunAnim : MonoBehaviour
+{
+    [SerializeField] public Weapon weapon;
+    [SerializeField] public Transform pivot1;
+    [SerializeField] public Transform pivot2;
+    [SerializeField] [Range(500, 1000)] public float speed;
+    
+    void Update()
+    {
+        if(weapon.firing)
+        {
+            pivot1.localEulerAngles += new Vector3(0, 0,  speed * Time.deltaTime);
+            pivot2.localEulerAngles += new Vector3(0, 0, -speed * Time.deltaTime);
+        }
+    }
+}
