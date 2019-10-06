6cb3416f32a4029321e1c72675ce11ca7b6764a6
Unknown <titwin@live.fr>
2019-10-06 17:10:30 +0200

    second weapon

--- Assets/Scripts/Combat/GuidingSystem.cs
+++ Assets/Scripts/Combat/GuidingSystem.cs
@@ -12,7 +12,6 @@ public class GuidingSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         bullet.targetPosition = targetPos;
         targetDisplay.gameObject.SetActive(false);
     }
