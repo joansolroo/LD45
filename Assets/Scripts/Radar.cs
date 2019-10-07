d798399fe6027efc7a61fb19ef5850187c9f4905
solroo <solroo@gmail.com>
2019-10-07 13:21:48 +0200

    radar UI

---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    [SerializeField] Sense sense;
+
+    [SerializeField] int maxSight;
+    [SerializeField] RadarArrow ArrowPrefab;
+    RadarArrow[] arrows;
+
+    [SerializeField] float minDistance = 0;
+    [SerializeField] float maxDistance = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrows = new RadarArrow[maxSight];
+        for(int a = 0; a < maxSight;++a)
+        {
+            RadarArrow arrow = GameElement.Instantiate<RadarArrow>(ArrowPrefab);
+            arrows[a] = arrow;
+            arrow.transform.parent = this.transform;
+            arrow.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        int c = 0;
+        foreach(GameObject p in sense.perceived.Keys)
+        {
+            if(c < maxSight)
+            {
+                if (p != null)
+                {
+                    arrows[c].gameObject.SetActive(true);
+                    arrows[c].transform.LookAt(p.transform);
+                    float d = Vector3.Distance(this.transform.position, p.transform.position);
+                    arrows[c].SetVisibility(1-(d/2 - minDistance) / (maxDistance - minDistance));
+                    ++c;
+                }
+            }
+        }
+        for(;c<maxSight;++c)
+        {
+            arrows[c].gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/RadarArrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarArrow : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer contour;
+    [SerializeField] SpriteRenderer background;
+    [SerializeField] float visibility = 0;
+
+
+    public void SetVisibility(float v)
+    {
+        visibility = v;
+        Color cc = contour.color;
+        cc.a = v;
+        contour.color = cc;
+
+        Color cb = background.color;
+        cb.a = v;
+        background.color = cb;
+    }
+}
