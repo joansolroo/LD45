2613aa9d23a64e5838b61c8aff360f513f7b93f9
solroo <solroo@gmail.com>
2019-10-07 02:26:58 +0200

    person is now aggressive

--- Assets/Scripts/Combat/DamagingObject.cs
+++ Assets/Scripts/Combat/DamagingObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DamagingObject : MonoBehaviour
 {
     [SerializeField] int damage = 1;
-
+    [SerializeField] float tickTime=1;
     //Vector3 position;
     //float randomStartWiggling;
     // Use this for initialization
@@ -19,9 +19,10 @@ public class DamagingObject : MonoBehaviour
       //  this.transform.localPosition = position + new Vector3(0, Mathf.Sin(Time.time * 6 + randomStartWiggling) * 0.05f, 0);
     }
 
-    
+    float t;
     void OnTriggerEnter(Collider other)
     {
+        t = 0;
        //Debug.Log("collision");
         if (other.gameObject.tag == "Player")
         {
@@ -32,6 +33,26 @@ public class DamagingObject : MonoBehaviour
                 controller.Push(-(this.transform.position - controller.transform.position));
             }
         }
+    }
 
+    private void OnTriggerStay(Collider other)
+    {
+       
+
+        if (other.gameObject.tag == "Player")
+        {
+            Controller controller = other.gameObject.GetComponent<Controller>();
+            if (controller != null)
+            {
+                t += Time.deltaTime;
+                if (t > tickTime)
+                {
+                    t -= tickTime;
+                    controller.Damage(damage);
+                    controller.Push(-(this.transform.position - controller.transform.position));
+                }
+            }
+        }
+        
     }
 }
