ff1d8b59a35006a74e4c2b235da72ae2168a2af6
Unknown <titwin@live.fr>
2019-10-12 15:34:30 +0200

    add arrow to boss

---
+++ Assets/Scripts/Animation/ArrowAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowAnim : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+    }
+}
--- Assets/Scripts/UI/BossTrigger.cs
+++ Assets/Scripts/UI/BossTrigger.cs
@@ -7,10 +7,13 @@ public class BossTrigger : MonoBehaviour
     [Range(1, 30)] public float detectionRange;
     public Transform player;
     public bool trigger = false;
+    public GameObject[] triggerListener;
 
     // Update is called once per frame
     void Update()
     {
         trigger = (player.position - transform.position).magnitude <= detectionRange;
+        foreach (GameObject go in triggerListener)
+            go.SetActive(!trigger);
     }
 }
